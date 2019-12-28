8ec71654def9cbf650ce4d41bd21b024bd66cda9
BuildTools <rp_ke@yahoo.co.jp>
2019-12-28 12:56:40 +0900

    小節ごとに構成音をboolean配列で保持するようにした

--- src/rp/ken/intellichord/MidiImportManager.java
+++ src/rp/ken/intellichord/MidiImportManager.java
@@ -2,6 +2,7 @@ package rp.ken.intellichord;
 
 import java.io.File;
 import java.util.ArrayList;
+import java.util.Arrays;
 
 import javax.sound.midi.MidiEvent;
 import javax.sound.midi.MidiMessage;
@@ -21,6 +22,8 @@ public class MidiImportManager {
 		// 1拍は480tick 1拍は60/BPM秒なので10tick = 1250/BPM[ms]
 
 		int bpm = 142;
+		ArrayList<Integer> chords = new ArrayList<>();
+		ArrayList<boolean[]> chordsBool = new ArrayList<>();
 		int trackNumber = 0;
 		ArrayList<ArrayList<String>> melody = new ArrayList<>();
 		ArrayList<ArrayList<Integer>> noteDurations = new ArrayList<>();
@@ -44,24 +47,45 @@ public class MidiImportManager {
 						note = key % 12;
 						noteName = NOTE_NAMES[note];
 						onTime = event.getTick();
+						if (chords.size() > (int) (onTime / 1920l)) {//十分
+							chords.set((int) (onTime / 1920l),
+							        chords.get((int) (onTime / 1920l)) + (int) Math.pow(2, note));
+							boolean[] tmp = chordsBool.get((int) (onTime / 1920l));
+							tmp[note] = true;
+							chordsBool.set((int) (onTime / 1920l), tmp);
+						} else {//足りない
+							chords.add((int) Math.pow(2, note));
+							boolean[] tmp = new boolean[12];
+							tmp[note] = true;
+							chordsBool.add(tmp);
+						}
 						if (onTime - offTime != 0) {
-							System.out.println(offTime + " A0 " + (onTime - offTime));
+							System.out.print(offTime + " A0 " + (onTime - offTime));
 							tmpm.add("NO");
 							tmpd.add((int) ((onTime - offTime) * 125 / bpm));
 						}
 						System.out.print(onTime + " " + noteName + octave + " ");
 					} else if (sm.getCommand() == NOTE_OFF) {
 						offTime = event.getTick();
-						System.out.println((offTime - onTime));
+						System.out.print((offTime - onTime));
 						tmpm.add((noteName + octave));
 						tmpd.add((int) ((offTime - onTime) * 125 / bpm));
 					}
+					System.out.println();
 				}
 			}
 			int tmp = tmpd.size();
 			tmpd.add(tmp);
 			melody.add(tmpm);
 			noteDurations.add(tmpd);
+		}
+		System.out.println();
+		for (int i = 0; i < chords.size(); i++) {
+			System.out.println(chords.get(i));
+		}
+		System.out.println();
+		for (int i = 0; i < chords.size(); i++) {
+			System.out.println(Arrays.toString(chordsBool.get(i)));
 		}
 		System.out.println(melody);
 		System.out.println(noteDurations);
